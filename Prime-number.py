81c38ff9e5149a5f923778eae39b560d062aefef
JongEun Lee <bellsilver7@gmail.com>
2022-07-19 07:37:48 +0900

    Update Prime-number.py

--- Prime-number.py
+++ Prime-number.py
@@ -1,6 +1,8 @@
 import math
 
 def isPrimeNumber(num):
+    if num == 1: 
+        return False
     # 2부터 x의 제곱근까지의 모든 수를 확인
     for i in range(2, int(math.sqrt(num)) + 1):
         # x가 해당 수로 나누어떨어진다면
